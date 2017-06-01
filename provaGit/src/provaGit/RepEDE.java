ad2efa2ecc24a521d3b58b5b0ca47e4a28c027bc
sergix1 <sergigandia1@gmail.com>
2017-06-01 15:11:29 +0200

    gg
    gg

---
+++ provaGit/src/provaGit/RepEDE.java
@@ -0,0 +1,10 @@
+package provaGit;
+
+public class RepEDE {
+	public static void main(String[] args){
+	System.out.println ("Hello World.");
+	 System.out.println ("IDE Eclipse");
+	 System.out.println ("Modificacio desde eclipse");
+
+	}
+}
